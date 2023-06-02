4aa67dab109e7b5b013d05b53058c61607979813
Dainius <dainiusnarkevicius1@gmail.com>
2023-06-02 12:06:23 +0300

    2 uzduotis

---
+++ uzduotys/2uzduotis.js
@@ -0,0 +1,11 @@
+console.clear();
+
+const vardas = 'Dainius';
+const pavarde = 'Narkevičius';
+const gimimoMetai = 1996;
+const metai = 2023; 
+
+const dabartinisAmzius = metai - gimimoMetai;
+
+console.log(`As esu ${vardas} ${pavarde}. Man yra ${dabartinisAmzius} metai.`);
+
