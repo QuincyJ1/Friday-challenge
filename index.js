2bdba0f38ef7d46ca8bf934270dbacc4b70ba830
Dainius <dainiusnarkevicius1@gmail.com>
2023-06-02 11:49:59 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.clear();
+
+const actor1 = "Robert De Niro";
+const actor2 = "Margot Robbie";
+
+if (actor1.length < actor2.length) {
+    console.log(actor1);
+} else {
+    console.log(actor2);
+}
+
