07af69f8d2c561e25aede6574b2a01e8a287f55e
Dainius <dainiusnarkevicius1@gmail.com>
2023-06-02 12:16:21 +0300

    3 uzduotis

---
+++ uzduotys/3uzduotis.js
@@ -0,0 +1,9 @@
+console.clear();
+
+const firstName = 'Robert';
+const lastName = 'De Niro';
+
+const newName = firstName[3] + firstName[4] + firstName[5] + lastName[4] + lastName[5] + lastName[6];
+console.log(newName);
+
+
